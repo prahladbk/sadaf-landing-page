4408e3764dd2c211775a329716b8de3afc7ee098
Prahlad bk <prahladbk50@gmail.com>
2024-09-30 22:12:28 +0530

    small changes

--- src/components/about.container.jsx
+++ src/components/about.container.jsx
@@ -5,13 +5,15 @@ import lexus_logo from "../assets/lexus-logo.png";
 function About() {
   return (
     <div className="mx-4">
-      <h1>About</h1>
-      <div className="about flex">
-        <div className="flex">
-        <img className="w-34"src={lexus_logo} alt="Lexus log" />
-        <img className="w-34"src={toyota_logo} alt="Toyota log" />
-        </div>
-        <div>
+      <div className="flex">
+        <h1 className="mx-4 font-semibold text-xl hover:font-extrabold group transition duration-300 ease-in-out">
+          <span className="text-2xl bg-left-bottom bg-gradient-to-r from-blue-900 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+            About
+          </span>
+        </h1>
+      </div>
+      <div className="md:flex-row-reverse">
+        <div className="text-wrap max-w-lg text-center mx-auto mt-4">
           Sadaf Al Arabia, an Oman based Automotive spare parts Company for
           Japanese and Korean model vehicles, Specialized in Retail sales. We
           have over 13 years of experience in sales for Japanese Genuine Parts.
@@ -21,6 +23,10 @@ function About() {
           competitive pricing. Delivering quality spare parts of leading
           Japanese and Korean car brands, we provide you the comfort with an
           exemplary customer service.
+        </div>
+        <div className="flex space-x-3 justify-center">
+          <img className="w-32" src={lexus_logo} alt="Lexus log" />
+          <img className="w-32" src={toyota_logo} alt="Toyota log" />
         </div>
       </div>
     </div>
